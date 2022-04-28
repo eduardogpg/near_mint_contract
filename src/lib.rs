d487e5423890bba8fbad38badf49e737e6bb15e4
eduardogpg <eduardo78d@gmail.com>
2022-04-27 20:29:08 -0500

    v2

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 // DOCUMENTATION
 // NEP - 177 Standar
-
+// https://nomicon.io/Standards/Tokens/NonFungibleToken/Metadata
 
 use near_contract_standards::non_fungible_token::metadata::{
-    NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
+    NFTContractMetadata, TokenMetadata, NFT_METADATA_SPEC,
 };
 use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_contract_standards::non_fungible_token::NonFungibleToken;
@@ -14,19 +14,20 @@ use near_sdk::json_types::Base64VecU8;
 use near_sdk::json_types::ValidAccountId;
 
 use near_sdk::{
-    env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,  
+    env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, PromiseOrValue,  
 };
 
-near_sdk::setup_alloc!();
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
-    tokens: NonFungibleToken,
+    token: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
 }
 
-const ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+near_contract_standards::impl_non_fungible_token_core!(Contract, token);
+
+const ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M18.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
@@ -41,34 +42,27 @@ enum StorageKey {
 impl Contract {
     
     #[init]
-    pub fn new_default_meta(owner_id: ValidAccountId, name: String, symbol: String) -> Self {
-        Self::new(
-            owner_id,
-            NFTContractMetadata {
-                spec: NFT_METADATA_SPEC.to_string(),
-                name: name,
-                symbol: symbol,
-                icon: Some(ICON.to_string()),
-                base_uri: Some("https://nft.storage/".to_string()),
-                reference: None,
-                reference_hash: None,
-            },
-        )
-    }
-
-    #[init]
-    pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata) -> Self {
-        assert!(!env::state_exists(), "Contract already initialized");
-        metadata.assert_valid();
+    pub fn new(owner_id: ValidAccountId, name: String, symbol: String) -> Self {
         Self {
-            tokens: NonFungibleToken::new(
+            token: NonFungibleToken::new(
                 StorageKey::NonFungibleToken,
                 owner_id,
                 Some(StorageKey::TokenMetadata),
                 Some(StorageKey::Enumeration),
-                Some(StorageKey::Approval),
+                Some(StorageKey::Approval)
             ),
-            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            metadata: LazyOption::new(
+                StorageKey::Metadata,
+                Some(&NFTContractMetadata {
+                    spec: NFT_METADATA_SPEC.to_string(),
+                    name: name,
+                    symbol: symbol,
+                    icon: Some(ICON.to_string()),
+                    base_uri: Some("https://nft.storage/".to_string()),
+                    reference: None,
+                    reference_hash: None,
+                })
+            ),
         }
     }
     
@@ -76,71 +70,25 @@ impl Contract {
     pub fn nft_mint(&mut self, token_id: TokenId, receiver_id: ValidAccountId, 
         title: String, description: String, media: String, hash: Base64VecU8) -> Token {
 
-        let token_metadata = self.generate_token_meta_data(title, description, media, hash);
-        self.tokens.mint(token_id, receiver_id, Some(token_metadata))
-    }
-
-    pub fn generate_token_meta_data(&self, title: String, description: String, media: String, hash: Base64VecU8) -> TokenMetadata{
-        TokenMetadata {
-            title: Some(title),
-            description: Some(description),
-            media: Some(media),
-            media_hash: Some(hash),
-            copies: Some(1),
-            issued_at: None,
-            expires_at: None,
-            starts_at: None,
-            updated_at: None,
-            extra: None, 
-            reference: None,
-            reference_hash: None 
-        }
+        let token_metadata = generate_token_meta_data(title, description, media, hash, 1);
+        self.token.mint(token_id, receiver_id, Some(token_metadata))
     }
 }
 
-near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
-near_contract_standards::impl_non_fungible_token_approval!(Contract, tokens);
-near_contract_standards::impl_non_fungible_token_enumeration!(Contract, tokens);
-
-#[near_bindgen]
-impl NonFungibleTokenMetadataProvider for Contract {
-    fn nft_metadata(&self) -> NFTContractMetadata {
-        self.metadata.get().unwrap()
-    }
-}
-
-#[cfg(all(test, not(target_arch = "wasm32")))]
-mod tests {
-    use near_sdk::test_utils::{accounts, VMContextBuilder};
-    use super::*;
-
-    const MINT_STORAGE_COST: u128 = 5870000000000000000000;
-
-    fn get_context(predecessor_account_id: ValidAccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder
-            .current_account_id(accounts(0))
-            .signer_account_id(predecessor_account_id.clone())
-            .predecessor_account_id(predecessor_account_id);
-        builder
-    }
-
-    fn sample_token_metadata() -> TokenMetadata {
-        TokenMetadata {
-            title: Some("Welcome NFT".into()),
-            description: Some("First NFT Team N°2".into()),
-            media: None,
-            media_hash: None,
-            copies: Some(1u64),
-            issued_at: None,
-            expires_at: None,
-            starts_at: None,
-            updated_at: None,
-            extra: None,
-            reference: None,
-            reference_hash: None,
-        }
-    }
-
+fn generate_token_meta_data(title: String, description: String, media: String, hash: Base64VecU8, copies: u64) -> TokenMetadata{
     
+    TokenMetadata {
+        title: Some(title),
+        description: Some(description),
+        media: Some(media),
+        media_hash: Some(hash),
+        copies: Some(copies),
+        issued_at: None,
+        expires_at: None,
+        starts_at: None,
+        updated_at: None,
+        extra: None, 
+        reference: None,
+        reference_hash: None 
+    }
 }
